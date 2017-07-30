e6b4ac9ed283163c88cfbe9888627947ef674741
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-07-29 19:31:24 -0700

    finished invokeMax function

--- callApplyBindSpec.test.js
+++ callApplyBindSpec.test.js
@@ -1,5 +1,7 @@
 let expect = require('chai').expect
 let { sumEvenArguments } = require('./call_apply_bind_exercise.js');
+let { arrayFrom } = require('./call_apply_bind_exercise.js');
+let { invokeMax } = require('./call_apply_bind_exercise.js');
 
 describe('#sumEvenArguments', function(){
   it('takes all of the arguments passed to a functin and returns the sum of the even ones', function(){
@@ -8,3 +10,28 @@ describe('#sumEvenArguments', function(){
     expect(sumEvenArguments(1,2)).to.equal(2);
   })
 })
+
+describe('#arrayFrom', function(){
+  function convert(){
+    var arr = arrayFrom(arguments)
+    return arr;
+  }
+  it("takes an array like object and converts it into an array", function(){
+    var argsArr = convert();
+    expect(argsArr).to.be.an('array');
+  });
+})
+
+describe("#invokeMax", function(){
+  function add(a,b){
+      return a+b
+  }
+  it("returns a function that calls another function a certain amount of times", function(){
+
+   var addOnlyThreeTimes = invokeMax(add,3);
+   expect(addOnlyThreeTimes(1,2)).to.equal(3) // 3
+   expect(addOnlyThreeTimes(2,2)).to.equal(4) // 4
+   expect(addOnlyThreeTimes(1,2)).to.equal(3) // 3
+   expect(addOnlyThreeTimes(1,2)).to.equal("Maxed Out!")
+  });
+});
--- call_apply_bind_exercise.js
+++ call_apply_bind_exercise.js
@@ -8,6 +8,55 @@ function sumEvenArguments(){
       }
     }, 0);
 }
-console.log(sumEvenArguments(1,2,6));
 
-module.exports = { sumEvenArguments, }
+function arrayFrom(){
+  return [].slice.call(arguments);
+}
+
+var obj = {
+    fullName: "Harry Potter",
+    person: {
+        sayHi: function(){
+            return "This person's name is " + this.fullName
+        }
+    }
+}
+
+function invokeMax(fn, number){
+  var maxOut = 0;
+  return function (){
+    maxOut++;
+    if(maxOut > number) return 'Maxed Out!'
+    var arr = Array.from(arguments);
+    return fn.apply(this, arr);
+  }
+}
+
+function guessingGame(amount){
+  var answer = Math.floor(Math.random() * 11);
+  var guesses = 0;
+  var completed = false;
+  return function(guess){
+    if(!completed){
+      var option;
+      guesses++;
+      if(guess === answer){
+        completed = true;
+        option = "You got it!"
+      }
+      if(guess > answer ) option = "You are too high!";
+      if(guess < answer ) option = "You are too low!";
+      if(guesses === amount ** guess !== answer){
+        completed = true;
+        return "No more guesses the answer was " + answer;
+      }
+      return option;
+    }
+    return "You are all done playing!"
+  }
+}
+// console.log(obj.person.sayHi.bind(obj)())
+// console.log(arrayFrom(1,2,4,5));
+// console.log(sumEvenArguments(1,2,6));
+
+module.exports = { sumEvenArguments,  arrayFrom, invokeMax }
